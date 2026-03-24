cb21816ad30609772d6d30a7d6f3f1ffa1d72fc2
ByteBuddy2000 <bytebuddy.dev@gmail.com>
2026-03-24 10:20:10 +0100

    Modify Tawk component to change script URL
    Updated Tawk.to script URL and removed chat widget rendering.

--- components/Tawk.jsx
+++ components/Tawk.jsx
@@ -1,29 +1,23 @@
 "use client";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 const Tawk = () => {
   useEffect(() => {
-    // Dynamically inject the Tawk.to script
-    var Tawk_API = Tawk_API || {};
-    var Tawk_LoadStart = new Date();
+    window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
     (function () {
       var s1 = document.createElement("script");
       var s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = 'https://embed.tawk.to/687b8683487057192063b18c/1j0h8at6r';
+      s1.src = "https://embed.tawk.to/69befbeb1f2eee1c3a8ff614/1jk90g6sb";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
     })();
-  }, []); // Empty dependency array ensures this runs only once after the component mounts
+  }, []);
 
-  return (
-    <div className="z-[100] bottom-7"
-      title="Chat with us"
-    >
-      {/* Tawk.to chat widget will be injected here */}
-    </div>
-  );
+  return null;
 };
 
 export default Tawk;
